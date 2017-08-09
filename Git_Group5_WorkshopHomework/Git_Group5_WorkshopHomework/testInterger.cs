528071e332b87dde1ff5e81d911849f35891ca5c
26147629 <26147629@student.g.nwu.ac.za>
2017-08-09 07:35:46 +0200

    Klass wat julle moet gebruik.
    Maak n objek van die die testInteger form en roep dat testint ( int i)

---
+++ Git_Group5_WorkshopHomework/Git_Group5_WorkshopHomework/testInterger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Git_Group5_WorkshopHomework
+{
+    public class testInterger
+    {
+        public bool checkint(int i)
+        {
+            try
+            {
+                if (i > 5 && i < 20)
+                    return true;
+                else return false;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
